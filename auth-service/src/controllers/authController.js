f18e3293bde424d42f7523ffe8582bc93bb7693d
Nguyễn Toàn <ttoan.nguyen2910@gmail.com>
2024-11-14 14:35:02 +0700

    samesite cookie

--- auth-service/src/controllers/authController.js
+++ auth-service/src/controllers/authController.js
@@ -23,8 +23,7 @@ const createSendToken = async (statusCode, user, res, isRedirect) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true,
-    sameSite: 'Lax'
+    httpOnly: true
   };
 
   if (process.env.NODE_ENV === 'production') {
